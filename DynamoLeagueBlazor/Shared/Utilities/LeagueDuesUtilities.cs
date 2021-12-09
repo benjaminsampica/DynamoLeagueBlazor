e2da6a406c0a04f9a39407a0e4f388854c074535
Ben Sampica <benjamin.sampica@gmail.com>
2021-12-09 13:57:14 -0600

    Add Rules

---
+++ DynamoLeagueBlazor/Shared/Utilities/LeagueDuesUtilities.cs
@@ -0,0 +1,15 @@
+﻿namespace DynamoLeagueBlazor.Shared.Utilities;
+
+public static class LeagueDuesUtilities
+{
+    private static readonly int _startYear = 2020;
+    private static readonly int _startDues = 105;
+    private static readonly int _yearlyIncrease = 5;
+
+    public static int GetCurrentLeagueDues(DateOnly date)
+    {
+        var dueIncrease = (date.Year - _startYear) * _yearlyIncrease;
+
+        return _startDues + dueIncrease;
+    }
+}
---
+++ DynamoLeagueBlazor/Shared/Utilities/SalaryCapUtilities.cs
@@ -0,0 +1,13 @@
+﻿namespace DynamoLeagueBlazor.Shared.Utilities;
+
+public static class SalaryCapUtilities
+{
+    const int StartYear = 2020;
+    const int BaseSalaryCap = 1050;
+    const int BiYearlyCapIncrease = 50;
+
+    public static int GetCurrentCapValue(DateOnly date)
+    {
+        return (date.Year - StartYear) / 2 * BiYearlyCapIncrease + BaseSalaryCap;
+    }
+}
