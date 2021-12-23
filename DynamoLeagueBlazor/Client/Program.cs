5ef6f1e45d70d8047a6b46d8b816acb2ad2e1347
Ben Sampica <benjamin.sampica@gmail.com>
2021-12-22 20:51:45 -0600

    Add serilog.

--- DynamoLeagueBlazor/Client/Program.cs
+++ DynamoLeagueBlazor/Client/Program.cs
@@ -5,23 +5,49 @@ using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor;
 using MudBlazor.Services;
+using Serilog;
+using Serilog.Core;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddHttpClient("DynamoLeagueBlazor.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
-// Supply HttpClient instances that include access tokens when making requests to the server project
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("DynamoLeagueBlazor.ServerAPI"));
-
-builder.Services.AddApiAuthorization()
-    .AddAccountClaimsPrincipalFactory<ApplicationUserFactory>();
-
-builder.Services.AddMudServices(config =>
+try
 {
-    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
-});
+    var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-await builder.Build().RunAsync();
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+
+    builder.Services.AddHttpClient("DynamoLeagueBlazor.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+        .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+    // Supply HttpClient instances that include access tokens when making requests to the server project
+    builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("DynamoLeagueBlazor.ServerAPI"));
+
+    builder.Services.AddApiAuthorization()
+        .AddAccountClaimsPrincipalFactory<ApplicationUserFactory>();
+
+    builder.Services.AddMudServices(config =>
+    {
+        config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
+    });
+
+    var host = builder.Build();
+
+    var levelSwitch = new LoggingLevelSwitch();
+    Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.ControlledBy(levelSwitch)
+        .WriteTo.BrowserHttp($"{builder.HostEnvironment.BaseAddress}ingest",
+            controlLevelSwitch: levelSwitch,
+            messageHandler: host.Services.GetRequiredService<BaseAddressAuthorizationMessageHandler>()
+        ).WriteTo.BrowserConsole()
+        .CreateLogger();
+
+    await host.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "An exception occurred while creating the WASM host.");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
--- DynamoLeagueBlazor/Server/Program.cs
+++ DynamoLeagueBlazor/Server/Program.cs
@@ -6,106 +6,127 @@ using DynamoLeagueBlazor.Shared.Features.Players;
 using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Serilog;
 using System.IdentityModel.Tokens.Jwt;
 using System.Reflection;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+try
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-});
-builder.Services.AddDbContextFactory<ApplicationDbContext>(lifetime: ServiceLifetime.Scoped);
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRoles<ApplicationRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddClaimsPrincipalFactory<CurrentUserClaimsFactory>();
+    Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .Enrich.FromLogContext()
+        .Enrich.WithProperty("Environment", builder.Environment.EnvironmentName)
+        .CreateLogger();
 
-builder.Services.AddIdentityServer()
-    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+    builder.Host.UseSerilog();
 
-builder.Services.AddTransient<IProfileService, ProfileService>();
-JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("role");
-
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.User.RequireUniqueEmail = true;
-});
-
-builder.Services.AddAuthentication()
-    .AddIdentityServerJwt();
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
-builder.Services.AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<AddFineRequestValidator>());
-
-builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection(EmailSettings.Email))
-    .AddSingleton(s => s.GetRequiredService<IOptions<EmailSettings>>().Value);
-
-if (builder.Environment.IsProduction())
-{
-    builder.Services.AddSingleton<IEmailSender, EmailSender>();
-}
-else
-{
-    builder.Services.AddSingleton<IEmailSender, DevelopmentEmailSender>();
-}
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseIdentityServer();
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
-await using (var scope = app.Services.CreateAsyncScope())
-{
-    var applicationDbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-
-    if (app.Environment.IsDevelopment())
+    // Add services to the container.
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
     {
-        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
-        await mediator.Send(new SeedDataCommand());
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+        options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    });
+    builder.Services.AddDbContextFactory<ApplicationDbContext>(lifetime: ServiceLifetime.Scoped);
+
+    builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+        {
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.User.RequireUniqueEmail = true;
+            options.SignIn.RequireConfirmedAccount = true;
+        })
+        .AddRoles<ApplicationRole>()
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddClaimsPrincipalFactory<CurrentUserClaimsFactory>();
+
+    builder.Services.AddIdentityServer()
+        .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+    builder.Services.AddTransient<IProfileService, ProfileService>();
+    JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("role");
+
+    builder.Services.AddAuthentication()
+        .AddIdentityServerJwt();
+
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddRazorPages();
+
+    builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+    builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+    builder.Services.AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<AddFineRequestValidator>());
+
+    builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection(EmailSettings.Email))
+        .AddSingleton(s => s.GetRequiredService<IOptions<EmailSettings>>().Value);
+
+    if (builder.Environment.IsProduction())
+    {
+        builder.Services.AddSingleton<IEmailSender, EmailSender>();
+    }
+    else
+    {
+        builder.Services.AddSingleton<IEmailSender, DevelopmentEmailSender>();
     }
 
-    await applicationDbContext.Database.MigrateAsync();
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseWebAssemblyDebugging();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseBlazorFrameworkFiles();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseIdentityServer();
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.UseSerilogIngestion();
+    app.UseSerilogRequestLogging();
+
+    app.MapRazorPages();
+    app.MapControllers();
+    app.MapFallbackToFile("index.html");
+
+    await using (var scope = app.Services.CreateAsyncScope())
+    {
+        var applicationDbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        if (app.Environment.IsDevelopment())
+        {
+            var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+            await mediator.Send(new SeedDataCommand());
+        }
+
+        await applicationDbContext.Database.MigrateAsync();
+    }
+
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "An exception occurred while creating the API host.");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-await app.RunAsync();
