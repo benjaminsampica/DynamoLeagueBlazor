fab785df3d6aead16d61550dd6e4034dc8b5702e
Ben Sampica <benjamin.sampica@gmail.com>
2021-12-27 21:02:14 -0600

    Change admin to be like user

--- DynamoLeagueBlazor/Tests/IntegrationTesting.cs
+++ DynamoLeagueBlazor/Tests/IntegrationTesting.cs
@@ -184,16 +184,24 @@ internal class UserAuthenticationHandler : AuthenticationHandler<AuthenticationS
 internal class AdminAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
 {
     public const string AuthenticationName = RoleName.Admin;
-    public const int TeamId = 1;
+    public static int TeamId = 1;
+    private readonly ApplicationDbContext _applicationDbContext;
 
-    public AdminAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options,
-        ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+    public AdminAuthenticationHandler(
+        IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder,
+        ISystemClock clock,
+        ApplicationDbContext applicationDbContext)
         : base(options, logger, encoder, clock)
     {
+        _applicationDbContext = applicationDbContext;
     }
 
-    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
     {
+        TeamId = (await _applicationDbContext.Teams.FirstOrDefaultAsync())?.Id ?? TeamId;
+
         var claims = new[] {
             new Claim(ClaimTypes.Name, RandomString),
             new Claim(ClaimTypes.Role, RoleName.Admin),
@@ -205,6 +213,6 @@ internal class AdminAuthenticationHandler : AuthenticationHandler<Authentication
 
         var result = AuthenticateResult.Success(ticket);
 
-        return Task.FromResult(result);
+        return result;
     }
 }
