4724896555d70ef99c968e31d24c815bd526d31b
Ben Sampica <benjamin.sampica@gmail.com>
2022-02-01 22:26:35 -0600

    Add submit loading button

--- DynamoLeagueBlazor/Client/Features/Admin/StartSeason.razor.cs
+++ DynamoLeagueBlazor/Client/Features/Admin/StartSeason.razor.cs
@@ -13,7 +13,6 @@ public partial class StartSeason : IDisposable
     [Inject] private ISnackbar SnackBar { get; set; } = null!;
 
     private bool _isDisabled = true;
-    private bool _isProcessing;
     private const string _title = "Start Season";
     private readonly CancellationTokenSource _cts = new();
 
@@ -24,13 +23,11 @@ public partial class StartSeason : IDisposable
 
     private async Task StartSeasonAsync()
     {
-        _isProcessing = true;
         await HttpClient.PostAsync("api/admin/startseason", null);
 
         SnackBar.Add("A new season has begun!", Severity.Success);
 
         _isDisabled = await GetSeasonStatusAsync();
-        _isProcessing = false;
     }
 
     private async Task<bool> GetSeasonStatusAsync()
---
+++ DynamoLeagueBlazor/Tests/Shared/Components/LoadingButtonTests.cs
@@ -0,0 +1,33 @@
+﻿using DynamoLeagueBlazor.Client.Shared.Components;
+using MudBlazor;
+
+namespace DynamoLeagueBlazor.Tests.Shared.Components;
+
+public class LoadingButtonTests : UITestBase
+{
+    [Fact]
+    public void WhenIsFirstRendered_ThenShowsSaveAndIsEnabled()
+    {
+        var component = RenderComponent<LoadingButton>();
+
+        component.Markup.Should().Contain("Save");
+
+        var button = component.FindComponent<MudButton>();
+
+        button.Instance.Disabled.Should().BeFalse();
+    }
+
+    [Fact]
+    public void WhenIsLoading_ThenShowsLoadingContentAndDisablesButton()
+    {
+        var component = RenderComponent<LoadingButton>(parameters =>
+        {
+            parameters.Add(p => p.IsLoading, true);
+        });
+
+        component.Markup.Should().Contain("Please wait...");
+        var button = component.FindComponent<MudButton>();
+
+        button.Instance.Disabled.Should().BeTrue();
+    }
+}
