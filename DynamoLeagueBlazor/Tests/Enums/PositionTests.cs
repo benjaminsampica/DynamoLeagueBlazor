84c9ed681fabb07f7036ca02d72e39623e42822a
morkusporkus <bearsss111@gmail.com>
2022-03-04 12:19:51 -0600

    clean up

--- DynamoLeagueBlazor/Tests/Enums/PositionTests.cs
+++ DynamoLeagueBlazor/Tests/Enums/PositionTests.cs
@@ -12,14 +12,6 @@ namespace DynamoLeagueBlazor.Tests.Enums
         [Theory]
         [InlineData("QB", 1,0,1)]
         [InlineData("QB", 1, 5, 150)]
-        [InlineData("RB", 1, 0, 1)]
-        [InlineData("RB", 1,5,300)]
-        [InlineData("WR", 1, 0, 1)]
-        [InlineData("WR", 1,5,150)]
-        [InlineData("TE", 1, 0, 1)]
-        [InlineData("TE", 1,5,145)]
-        [InlineData("DEF", 1, 0, 1)]
-        [InlineData("DEF", 1,5,6)]
         public void GivenBidValueAndPosition_ThenReturnsContractAmount(string positionValue,int bidValue,int chartElement,int minimumAmount)
         {
            var position = Position.FromName(positionValue);
