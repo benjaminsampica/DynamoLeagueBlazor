722ee6d8add8b2559b3f7522128f32dc2c339979
Ben Sampica <benjamin.sampica@gmail.com>
2022-06-19 15:54:03 -0500

    Provide an alternative when a player headshot is missing. (#92)
    * Provide an alternative when a player headshot is missing.
    
    * Update Avatar.razor
    
    * Update AvatarTests.cs

---
+++ src/Tests/Shared/Components/AvatarTests.cs
@@ -0,0 +1,25 @@
+﻿using DynamoLeagueBlazor.Client.Shared.Components;
+
+namespace DynamoLeagueBlazor.Tests.Shared.Components;
+
+public class AvatarTests : UITestBase
+{
+    [Fact]
+    public void GivenNoImageUrl_ThenIsAnIcon()
+    {
+        var component = RenderComponent<Avatar>();
+
+        component.Markup.Should().Contain("icon");
+    }
+
+    [Fact]
+    public void GivenAnImageUrl_ThenIsAnAvatar()
+    {
+        var component = RenderComponent<Avatar>(parameters =>
+        {
+            parameters.Add(p => p.Image, RandomString);
+        });
+
+        component.Markup.Should().Contain("img");
+    }
+}
