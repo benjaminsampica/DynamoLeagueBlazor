02086160a5a047f35121334e622e18aab39f5e13
Ben Sampica <benjamin.sampica@gmail.com>
2022-07-07 21:24:07 -0500

    Add confirmation dialog for important things

--- src/Client/Features/Players/AddFine.razor.cs
+++ src/Client/Features/Players/AddFine.razor.cs
@@ -29,7 +29,7 @@ public sealed partial class AddFine : IDisposable
     {
         try
         {
-            _fineDetail = await HttpClient.GetFromJsonAsync<FineDetailResult>(FineDetailRouteFactory.Create(PlayerId), _cts.Token) ?? new();
+            _fineDetail = await HttpClient.GetFromJsonAsync<FineDetailResult>(FineDetailRouteFactory.Create(PlayerId), _cts.Token);
         }
         catch (AccessTokenNotAvailableException exception)
         {
---
+++ src/Client/Shared/Components/ConfirmDialog.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace DynamoLeagueBlazor.Client.Shared.Components;
+
+public partial class ConfirmDialog
+{
+    [CascadingParameter] MudDialogInstance MudDialog { get; set; } = null!;
+
+    private void Confirm() => MudDialog.Close(DialogResult.Ok(true));
+}
+
+public class ConfirmDialogService
+{
+    private readonly IDialogService _dialogService;
+
+    public ConfirmDialogService(IDialogService dialogService)
+    {
+        _dialogService = dialogService;
+    }
+
+    public async Task<bool> IsCancelledAsync()
+    {
+        var dialog = _dialogService.Show<ConfirmDialog>();
+        var result = await dialog.Result;
+
+        return result.Cancelled;
+    }
+}
