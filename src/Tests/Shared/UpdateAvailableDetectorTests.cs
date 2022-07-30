bc8d31341bca95a42184b1cdb7048a9943f07342
Ben Sampica <benjamin.sampica@gmail.com>
2022-07-30 13:58:32 -0500

    Add tests

---
+++ src/Tests/Shared/UpdateAvailableDetectorTests.cs
@@ -0,0 +1,26 @@
+﻿using DynamoLeagueBlazor.Client.Shared;
+
+namespace DynamoLeagueBlazor.Tests.Shared;
+
+public class UpdateAvailableDetectorTests : UITestBase
+{
+    [Fact]
+    public void WhenNoUpdateIsDetected_ThenShowsNothing()
+    {
+        TestContext!.JSInterop.Setup<object>("registerForUpdateAvailableNotification", _ => true);
+        var component = RenderComponent<UpdateAvailableDetector>();
+
+        component.Markup.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task WhenAnUpdateIsDetected_ThenShowsAnAlert()
+    {
+        TestContext!.JSInterop.Setup<object>("registerForUpdateAvailableNotification", _ => true);
+        var component = RenderComponent<UpdateAvailableDetector>();
+
+        await component.Instance.OnUpdateAvailableAsync();
+
+        component.HasComponent<MudAlert>().Should().BeTrue();
+    }
+}
