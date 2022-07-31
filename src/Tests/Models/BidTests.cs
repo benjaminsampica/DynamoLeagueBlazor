0c53af2d411c949d7cc848e2365ab6f2bc548e86
Ben Sampica <benjamin.sampica@gmail.com>
2022-07-31 12:48:53 -0500

    add unit tests for model classes

---
+++ src/Tests/Models/BidTests.cs
@@ -0,0 +1,25 @@
+﻿namespace DynamoLeagueBlazor.Tests.Models;
+
+public class BidExtensionsTests
+{
+    [Fact]
+    public void FindHighestBid_GivenNoBids_ThenReturnsNothing()
+    {
+        var bids = new List<Bid>();
+
+        bids.FindHighestBid().Should().BeNull();
+    }
+
+    [Fact]
+    public void FindHighestBid_GivenTwoBids_ThenReturnsTheHighestOne()
+    {
+        var bids = new List<Bid>()
+        {
+            new(1, int.MaxValue, int.MaxValue),
+            new(2, int.MaxValue, int.MaxValue)
+        };
+
+        bids.FindHighestBid().Should().NotBeNull();
+        bids.FindHighestBid()!.Amount.Should().Be(2);
+    }
+}
